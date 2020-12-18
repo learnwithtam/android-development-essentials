0f6657fbcc72bb651f571ba7865e2529ffd4ffc6
learnwithtam <learnwithtam@gmail.com>
2020-12-18 13:09:54 +0530

    initial commit

---
+++ 03 - Clickocounter/app/src/main/java/com/example/click_o_counter/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.click_o_counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var countText: TextView
+    private lateinit var clickMeButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        countText = findViewById(R.id.countText)
+        clickMeButton = findViewById(R.id.clickMeButton)
+
+        clickMeButton.setOnClickListener{
+            clicked()
+
+        }
+    }
+
+    fun clicked(){
+        var currentCount = countText.text.toString().toInt()
+        var newCount = currentCount + 1
+        countText.text = newCount.toString()
+    }
+}
